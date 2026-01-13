d2976497802e2cb941a9e046ecaf8fe01e176036
hahyt6644-gif <hahyt6644@gmail.com>
2026-01-13 16:05:29 +0530

    Update puppeteer.config.cjs

--- puppeteer.config.cjs
+++ puppeteer.config.cjs
@@ -4,6 +4,5 @@ const { join } = require('path');
  * @type {import("puppeteer").Configuration}
  */
 module.exports = {
-  // Tells Puppeteer to install the browser inside your project folder
   cacheDirectory: join(__dirname, '.cache', 'puppeteer'),
 };
